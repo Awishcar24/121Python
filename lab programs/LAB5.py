af12c168e2e64d569570a9fcaa0fdaefe005e5c2
Awishcar24 <135306849+Awishcar24@users.noreply.github.com>
2023-09-03 10:03:15 +0530

    Update LAB5.py

--- lab programs/LAB5.py
+++ lab programs/LAB5.py
@@ -1,11 +1,12 @@
-testFile = open('C:\\Users\\Student\\Desktop\\test.txt', 'r') testContent = testFile.read()
-word_list =
-testContent.split()
-char_dict = {} # Empty dictionary for char in word_list:
+testFile = open('C:\\Users\\Student\\Desktop\\test.txt', 'r')
+testContent = testFile.read()
+word_list =testContent.split()
+char_dict = {} 
+for char in word_list:
 if char in char_dict:
   char_dict [char] = char_dict [char] + 1
  else:
     char_dict [char] = 1
-sorted_dict sorted (char_dict.items (), key = lambda item: item [1], reverse = True)
+sorted_dict =sorted (char_dict.items (), key = lambda item: item [1], reverse = True)
 first10pairs list (sorted_dict) [:10]
 print (first10pairs)
