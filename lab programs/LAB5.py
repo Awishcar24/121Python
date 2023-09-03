9dd08845f8ed992605a5e520511b719aaddb2841
Awishcar24 <135306849+Awishcar24@users.noreply.github.com>
2023-09-03 10:03:49 +0530

    Update LAB5.py

--- lab programs/LAB5.py
+++ lab programs/LAB5.py
@@ -8,5 +8,5 @@ if char in char_dict:
  else:
     char_dict [char] = 1
 sorted_dict =sorted (char_dict.items (), key = lambda item: item [1], reverse = True)
-first10pairs list (sorted_dict) [:10]
+first10pairs= list (sorted_dict) [:10]
 print (first10pairs)
